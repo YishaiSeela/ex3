e3ed8bbc8714a6caea2071d36e366d6abb964f4a
yishaiSeela <yishai320ds@gmail.com>
2016-12-29 22:57:55 +0200

    move data from client to server

--- client.cpp
+++ client.cpp
@@ -1,75 +1,81 @@
-////
-//// Created by yanaiela on 12/10/16.
-////
-//#include <iostream>
-//#include <fstream>
-//#include <sstream>
-//#include <boost/archive/text_oarchive.hpp>
-//#include <boost/archive/text_iarchive.hpp>
-//#include <boost/tokenizer.hpp>
-//#include <boost/algorithm/string/predicate.hpp>
-//#include <boost/lexical_cast.hpp>
-//#include <boost/assign/list_of.hpp>
-//#include <boost/algorithm/string.hpp>
-//#include <boost/iostreams/device/back_inserter.hpp>
-////#include <boost/iostreams/stream.hpp>
-//#include <boost/archive/binary_oarchive.hpp>
-//#include <boost/archive/binary_iarchive.hpp>
-//#include <iostream>
-//#include "Udp.h"
-//#include "Parser.h"
-//#include "Driver.h"
 //
+// Created by yanaiela on 12/10/16.
 //
-//#include <unistd.h>
-//#include <iostream>
-//#include <stdlib.h>
-//
-//
-//using namespace std;
-//using namespace boost::archive;
-//std::stringstream ss;
-//
-//int main(int argc, char *argv[]) {
-//
-//    string input;
-//    Parser *prs1;
-//    string serialized;
-//    cout << argv[1] << endl;
-//    Udp udp(0, atoi(argv[1]));
-//    udp.initialize();
-//    //create driver
-//    cin >> input;
-//    prs1 = new Parser();
-//    prs1->parse(input, 5);
-//    int id = atoi(prs1->inputVector.at(0).c_str());
-//    int age = atoi(prs1->inputVector.at(1).c_str());
-//    char status = prs1->inputVector.at(2)[0];
-//    int experiance = atoi(prs1->inputVector.at(3).c_str());
-//    int vehicleId = atoi(prs1->inputVector.at(4).c_str());
-//    //create driver for serilization
-//    Driver *driver = new Driver(id, age, status, experiance, vehicleId);
-//    //serialization
-//    std::string serial_str;
-//    boost::iostreams::back_insert_device<std::string> inserter(serial_str);
-//    boost::iostreams::stream<boost::iostreams::back_insert_device<std::string> > s(inserter);
-//    boost::archive::binary_oarchive sendDriver(s);
-//    sendDriver << driver;
-//    udp.sendData(serialized);
-//    s.flush();
-//
-//    //save
-//    text_oarchive oa{ss};
-//    Driver *ab;
-//    Driver &rsave = *ab;
-//    oa << rsave;
-//
-//    //load
-//    text_iarchive ia{ss};
-//    Driver *a;
-//    Driver &rload = *a;
-//    ia >> rload;
-//    std::cout << "pass succeed" << '\n';
-//
-//    return 0;
-//}
+
+
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/tokenizer.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/assign/list_of.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/iostreams/device/back_inserter.hpp>
+#include <boost/iostreams/stream.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+
+#include <iostream>
+#include "Udp.h"
+#include "Parser.h"
+#include "Driver.h"
+#include <fstream>
+#include <sstream>
+
+#include <unistd.h>
+#include <stdlib.h>
+
+
+using namespace std;
+using namespace boost::archive;
+std::stringstream ss;
+
+int main(int argc, char *argv[]) {
+
+    string input;
+
+    Udp udp(0, atoi(argv[1]));
+    udp.initialize();
+
+    char buffer[1024];
+
+    cin >> input;
+
+    //udp.sendData(input);
+
+    Parser *prs1;
+    prs1 = new Parser();
+    prs1->parse(input, 5);
+    int id = atoi(prs1->inputVector.at(0).c_str());
+    int age = atoi(prs1->inputVector.at(1).c_str());
+    char status = prs1->inputVector.at(2)[0];
+    int experiance = atoi(prs1->inputVector.at(3).c_str());
+    int vehicleId = atoi(prs1->inputVector.at(4).c_str());
+    //create driver for serilization
+    Driver *driver = new Driver(id, age, status, experiance, vehicleId);
+    //serialization
+    std::string serial_str;
+    boost::iostreams::back_insert_device<std::string> inserter(serial_str);
+    boost::iostreams::stream<boost::iostreams::back_insert_device<std::string> > s(inserter);
+    boost::archive::binary_oarchive sendDriver(s);
+    sendDriver << driver;
+    udp.sendData(serial_str);
+    s.flush();
+
+    //save
+    text_oarchive oa{ss};
+    Driver *ab;
+    Driver &rsave = *ab;
+    oa << rsave;
+
+    //load
+    text_iarchive ia{ss};
+    Driver *a;
+    Driver &rload = *a;
+    ia >> rload;
+
+
+    std::cout << "pass succeed" << '\n';
+
+    return 0;
+}
--- server.cpp
+++ server.cpp
@@ -1,29 +1,194 @@
-//#include <iostream>
-//#include "Udp.h"
-//#include <unistd.h>
-//
-//int main() {
-//    std::cout << "Hello, from server" << std::endl;
-//
-//    Udp udp(1, 5555);
-//    udp.initialize();
-//
-//    char buffer[1024];
-//    udp.reciveData(buffer, sizeof(buffer));
-//    cout << buffer << endl;
-//    udp.sendData("sup?");
-//
-//
-////
-////    usleep(5000);
-//    Udp udp2(1, 5554);
-//    udp2.initialize();
-//
-//    char buffer2[1024];
-//    udp2.reciveData(buffer2, sizeof(buffer2));
-//    cout << buffer2 << endl;
-//    udp2.sendData("sup?");
-//
-//    // support more than one client?
-//    return 0;
-//}
+#include <iostream>
+#include "Udp.h"
+#include <unistd.h>
+#include <sstream>
+#include <list>
+
+#include "BfsSearch.h"
+#include "Grid.h"
+#include "StringParse.h"
+#include "string"
+#include "Parser.h"
+#include "Driver.h"
+#include "Ride.h"
+#include "OrderManager.h"
+
+int main() {
+
+    Udp udp(1, 1212);
+    udp.initialize();
+
+    char buffer[1024];
+    //udp.reciveData(buffer, sizeof(buffer));
+/*
+    char buffer[1024];
+    udp.reciveData(buffer, sizeof(buffer));
+    cout << buffer << endl;
+    udp.sendData("sup?");
+*/
+    Grid *g1 = new Grid();
+    string input;
+    OrderManager *om = new OrderManager;
+    int width;
+    int height;
+    int task;
+    int obstacles;
+    Point startPoint;
+    Point endPoint;
+    Point obstacle;
+    Parser *prs1, *prs2, *prs3, *prsOb;
+    BfsSearch *bfs;
+    Driver *driver;
+    Ride *ride;
+    Vehicle *vehicle;
+    //insert sizes of grid
+    cin >> width >> height;
+    //create grid
+    g1->createGrid(width,height);
+    //insert number of obstacles
+    cin >> obstacles;
+    //insert obstacle points
+    for (int i=0; i<obstacles; i++){
+        cin >> input;
+        prsOb = new Parser();
+        prsOb->parse(input, 2);
+        obstacle = Point(atoi(prsOb->inputVector.at(0).c_str()), atoi(prsOb->inputVector.at(1).c_str()));
+        g1->setObstacle(obstacle);
+        delete prsOb;
+    }
+    //insert task number
+    cin >> task;
+
+    while (true) {
+
+        switch (task) {
+            //task 1 - insert driver
+            case 1: {
+
+                cin >> input;
+
+                udp.reciveData(buffer, sizeof(buffer));
+
+                prs1 = new Parser();
+                prs1->parse(buffer, 5);
+                int id = atoi(prs1->inputVector.at(0).c_str());
+                int age = atoi(prs1->inputVector.at(1).c_str());
+                char status = prs1->inputVector.at(2)[0];
+                int experiance = atoi(prs1->inputVector.at(3).c_str());
+                int vehicleId = atoi(prs1->inputVector.at(4).c_str());
+                driver = new Driver(id, age, status, experiance, vehicleId);
+                om->addDriver(driver);
+                delete prs1;
+
+                cin >> task;
+                break;
+            }
+                //task 2 - insert ride
+            case 2: {
+
+                cin >> input;
+                prs2 = new Parser();
+                prs2->parse(input, 8);
+                int id = atoi(prs2->inputVector.at(0).c_str());
+                int passengers = atoi(prs2->inputVector.at(5).c_str());
+                double tarif = stod(prs2->inputVector.at(6).c_str());
+                float startTime = stof(prs2->inputVector.at(7).c_str());
+                startPoint = Point(atoi(prs2->inputVector.at(1).c_str()), atoi(prs2->inputVector.at(2).c_str()));
+                endPoint = Point(atoi(prs2->inputVector.at(3).c_str()), atoi((prs2->inputVector.at(4).c_str())));
+
+                g1->startPt = new Point(startPoint);
+                g1->endPt = new Point(endPoint);
+                bfs = new BfsSearch(g1);
+                bfs->runBfs();
+                bfs->printPath();
+                ride = new Ride(id, startPoint, endPoint, passengers, tarif,startTime, bfs->path);
+                delete bfs;
+                om->addRide(ride);
+                delete prs2;
+
+                cin >> task;
+                break;
+            }
+                //task 3 - insert a vehicle:
+            case 3: {
+                cin >> input;
+                prs3 = new Parser();
+                prs3->parse(input, 4);
+                int id = atoi(prs3->inputVector.at(0).c_str());
+                int taxiType = atoi(prs3->inputVector.at(1).c_str());
+                char manufacturer = prs3->inputVector.at(2)[0];
+                char color = prs3->inputVector.at(3)[0];
+                vehicle = new Vehicle(manufacturer, taxiType, id, color);
+                om->addCab(vehicle);
+                delete prs3;
+
+                cin >> task;
+                break;
+            }
+            case 4: {
+                //task 4 - request for a driver location:
+                cin >> input;
+                int driveId = atoi(input.c_str());
+                Point location = (om->getLocation(driveId));
+                cout << "(" << location.getXCoordinate() << "," << location.getYCoordinate() << ")" << endl;
+                cin >> task;
+
+                break;
+            }
+
+            case 6: {
+                //task 6 - move all drivers to their end point
+                int rides = om->listOfRides.size();
+                for (int i = 0; i < rides; i++) {
+                    if(i < om->listOfDrivers.size()) {
+                        g1->startPt = om->listOfRides[0]->getStartPoint();
+                        g1->endPt = om->listOfRides[0]->getEndPoint();
+                        endPoint = Point(g1->endPt->getXCoordinate(), g1->endPt->getYCoordinate());
+
+                        om->listOfDrivers[i]->setLocation(endPoint);
+                        om->listOfRides.erase(om->listOfRides.begin());
+                    }
+                }
+
+                cin >> task;
+                break;
+            }
+            case 7: {
+
+                //task 7 - delete all elemnts and exit the program
+                //g1->destroyGrid(); //- FIX IT!!!
+
+                delete g1;
+                delete om;
+                exit(0);
+
+            }
+            case 9:{
+
+                om->updateLOcations();
+
+                cin >> task;
+                break;
+
+            }
+            default: {
+                //for any other input - ask for a valid input
+                cin >> task;
+                break;
+            }
+        }
+    }
+
+/*
+//    usleep(5000);
+    Udp udp2(1, 5554);
+    udp2.initialize();
+
+    char buffer2[1024];
+    udp2.reciveData(buffer2, sizeof(buffer2));
+    cout << buffer2 << endl;
+    udp2.sendData("sup?");*/
+
+    // support more than one client?
+    return 0;
+}
